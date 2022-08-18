891d575ef78cd373c11a6a6cbff67c43dd2dfc3e
Bryan0066 <bgenest96@gmail.com>
2022-08-18 15:41:39 -0400

    Added "on submit", "on change" to submit

--- app/javascript/react/components/SessionNew.js
+++ app/javascript/react/components/SessionNew.js
@@ -3,30 +3,30 @@ import React, { useState, useEffect } from "react";
 import GuessNew from "./GuessNew";
 
 export const SessionNew = (props) => {
+  const[game, setGame] = useState([])
   const [guesses, setGuesses] = useState([]);
 
   const submitGuess = (event, formPayload) => {
-    setGuesses(formPayload);
+    let array = guesses.concat(formPayload)
+    event.preventDefault()
+    setGuesses(array);
   };
 
-let guessList
-
-if (guesses){  const guessList = guesses.map((guess)=>{
-      return(
-        <li>{guess}</li>
-      )
-  })}
+  let guessList = guesses.map((word)=>{
+    return<p>{word.guess}</p>
+  })
 
   return (
     <div className="game-card-container float-center">
       <h1 className="title">Wordy </h1>
+      <ul>{guessList}</ul>
       <div className="grid-x grid-margin-x">
         <div className="card small-auto">
           <div className="">
             <GuessNew submitGuess={submitGuess} />
           </div>
           <br/>
-          <ul>{guessList}</ul>
+          <ul></ul>
         </div>
       </div>
     </div>
