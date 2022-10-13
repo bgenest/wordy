676ef6f505d5dc3f1d68b19f1cf1e65b8d7c08c4
Bryan0066 <bgenest96@gmail.com>
2022-10-13 13:27:38 -0400

    added game option to homepage, styling

---
+++ app/javascript/react/components/GameTypeTile.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const GameTypeTile = (props) => {
+
+
+
+  return (
+    <div className="grid-container tile-container" id ="game-option-tile">
+    <div className="grid-x grid-padding-x grid-padding-y word-tile">
+      
+      <div className="cell card">
+
+          <div className="float-center" id="game-title"> {props.title}
+          </div>
+          <hr/>
+          <div id="game-description"> {props.description}
+          </div>
+            <a className="button-19 " role="button" href={props.url}>
+              let's go!
+            </a>
+      </div>
+    </div>
+
+  </div>
+  )
+}
+
+export default GameTypeTile
