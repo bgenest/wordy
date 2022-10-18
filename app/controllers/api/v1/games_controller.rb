842cb07dd0e610f88c02bd3fa614bf16b582b646
Bryan0066 <bgenest96@gmail.com>
2022-10-18 11:28:45 -0400

    added betters upport for daily game

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -2,9 +2,20 @@ class Api::V1::GamesController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def show
-    random_index = rand(Game.count + 1)
-    render json: Game.find(random_index)
+
+    daily_key = "99999"
+
+    if params['id'] == daily_key
+
+      render json: Game.find(daily_key)
+    else
+      
+      random_index = rand(Game.count + 1)
+      render json: Game.find(random_index)
+    end
+
   end
+
 
   def index
     render json: Game.all
